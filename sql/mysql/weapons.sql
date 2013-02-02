798c8c53705609cc76837d1746e4b67619d2e495
firefly2442 <firefly2442@gmail.com>
2013-02-02 17:37:23 -0600

    remove MySQL stored procedures from SQL file, these are no longer needed

--- sql/mysql/weapons.sql
+++ sql/mysql/weapons.sql
@@ -1,62 +1,11 @@
 
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
 --
 -- Database: `weapons`
 --
 CREATE DATABASE `weapons` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `weapons`;
 
-DELIMITER $$
---
--- Procedures
---
-CREATE DEFINER=`arma`@`localhost` PROCEDURE `CreateNewLoadOut`(IN u INT(20), IN n VARCHAR(255), IN a TEXT, IN b TEXT, IN c TEXT, IN d TEXT, IN e TEXT, IN f TEXT)
-BEGIN
-		INSERT into users (uid, name, ammo, weapons, backpackammo, backpackweapons, weapononback, oabackpack)
-		VALUES(
-                       u,n,a,b,c,d,e,f
-                       );
-    END$$
 
-CREATE DEFINER=`arma`@`localhost` PROCEDURE `DeleteLoadoutUsingUIDandName`(IN u INT(20), IN n VARCHAR(255))
-BEGIN
-		DELETE from users
-                WHERE uid = u AND name = n LIMIT 1;
-    END$$
-
-CREATE DEFINER=`arma`@`localhost` PROCEDURE `GetLoadoutByUIDandName`(IN u INT(20), IN n VARCHAR(255))
-BEGIN
-		SELECT uid, name, ammo, weapons, backpackammo, backpackweapons, weapononback, oabackpack
-		FROM users
-		WHERE uid = u AND name = n;
-    END$$
-
-CREATE DEFINER=`arma`@`localhost` PROCEDURE `GetLoadoutNamesUsingUID`(IN u INT(20))
-BEGIN
-		SELECT name
-		FROM users
-		WHERE uid = u;
-    END$$
-
-CREATE DEFINER=`arma`@`localhost` PROCEDURE `UpdateLoadoutUsingUIDandName`(IN u INT(20), IN n VARCHAR(255), IN a TEXT, IN b TEXT, IN c TEXT, IN d TEXT, IN e TEXT, IN f TEXT)
-BEGIN
-		UPDATE users
-		SET
-                      ammo = a,
-                      weapons = b,
-                      backpackammo = c,
-                      backpackweapons = d,
-                      weapononback = e,
-					  oabackpack = f
-                 WHERE uid = u AND name = n;
-    END$$
-
-DELIMITER ;
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
